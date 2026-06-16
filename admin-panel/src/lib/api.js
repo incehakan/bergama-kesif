1ccff3f1c7b13372476061671efbfd6e29e71fec
incehakan <hakanince10@gmail.com>
2026-06-16 06:59:30 +0300

    api url env'den oku

--- admin-panel/src/lib/api.js
+++ admin-panel/src/lib/api.js
@@ -3,8 +3,10 @@ import axios from 'axios'
 export const TOKEN_KEY = 'kesif_token'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
 })
+
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem(TOKEN_KEY)
